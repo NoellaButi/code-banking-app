e89c7463dcb450e77fe84062b7375bcdbcf45e10
Noella Buti <noellabuti@gmail.com>
2025-09-15 15:17:22 -0700

    Create wsgi.py

---
+++ app/wsgi.py
@@ -0,0 +1,13 @@
+# wsgi.py
+from __future__ import annotations
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"),
+        port=int(os.getenv("FLASK_RUN_PORT", "5000")),
+        debug=os.getenv("FLASK_DEBUG", "0") == "1",
+    )
