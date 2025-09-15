783e235b0256026923bf35c23a553ef0dd47903f
Noella Buti <noellabuti@gmail.com>
2025-09-15 14:48:38 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(
-        host = os.getenv("FLASK_RUN_HOST", "127.0.0.1"),
-        port = int(os.getenv("FLASK_RUN_PORT", "5000")),
-        debug = os.getenv("FLASK_DEBUG", "0") == "1",
+        host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"),
+        port=int(os.getenv("FLASK_RUN_PORT", "5000")),
+        debug=os.getenv("FLASK_DEBUG", "0") == "1",
     )
