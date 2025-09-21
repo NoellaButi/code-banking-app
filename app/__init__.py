6a3ea8dceea8fa1c49ea2a2beaaa3c478db897a7
Noella Buti <noellabuti@gmail.com>
2025-09-21 16:50:27 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,12 @@ def create_app(config_object: type[Config] = Config) -> Flask:
     from .auth import bp as auth_bp
     from .routes import bp as main_bp
     from .api import bp as api_bp
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
-    app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(api_bp)
 
     # --- make sure models are imported so migrations can detect them ---
-    from . import models
+    from . import models as models
 
     return app
